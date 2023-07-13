2e5b6be37bdf89400b31e043a3dfbfc1483b0eda
jeeyoung870 <76871404+jeeyoung870@users.noreply.github.com>
2023-07-13 15:16:01 +0900

    Time: 54 ms (82.96%), Space: 43.6 MB (7.32%) - LeetHub

---
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.js
@@ -0,0 +1,23 @@
+/**
+ * @param {string} word1
+ * @param {string} word2
+ * @return {string}
+ */
+
+
+
+var mergeAlternately =   function(word1, word2) {
+
+  var merged = '';
+    while(word1.length != 0 && word2.length != 0) { 
+        merged += (word1[0] + word2[0]);
+        word1 = word1.slice(1);
+        word2 = word2.slice(1);
+    }
+    merged += (word1+word2);
+    console.log(typeof merged);
+  
+    return merged;
+};
+
+
