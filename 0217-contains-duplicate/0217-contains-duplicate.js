47a8936e5d8e91a6f72b1cfcdfe9b9e4f33f21e1
jeeyoung870 <76871404+jeeyoung870@users.noreply.github.com>
2023-07-13 16:46:48 +0900

    Time: 185 ms (23.82%), Space: 51.6 MB (86.71%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var containsDuplicate = function(nums) {
+
+    var sortedNums = nums.sort();
+    var isDuplicate = false;
+
+    for(var i=0; i<sortedNums.length-1; i++) {
+        if(sortedNums[i] === sortedNums[i+1]) {
+            isDuplicate = true;
+            break;
+        }
+    }
+    return isDuplicate;
+};
