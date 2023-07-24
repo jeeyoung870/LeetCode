ef89f0d77b6c3fd3e167babb8bbb3f94a44679f0
jeeyoung870 <76871404+jeeyoung870@users.noreply.github.com>
2023-07-24 17:54:13 +0900

    Time: 66 ms (86.19%), Space: 43.6 MB (98.41%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.js
+++ 0125-valid-palindrome/0125-valid-palindrome.js
@@ -4,25 +4,22 @@
  */
 var isPalindrome = function(s) {
     
-    // time complexity : O(n+n/2)
+    // time complexity : O(n+n/2) ??
     // Space Complexity : O(n)
     
-    if(s.trim().length === 0){
-        return true;
-    }
-    
+    // if(s.trim().length === 0){
+    //     return true;
+    // }
+
     // \W ==  not [A-Za-z0-9_] 
-    var replacedStr = s.toLowerCase().replace(/\W|_/g, '');
-    //console.log(replacedStr);
+    var replacedStr = s.toLowerCase().replace(/[^a-z0-9]/g, '');
     
     const loop = Math.floor(replacedStr.length/2);
-    const len = replacedStr.length;
     
     for (var i=0; i<loop; i++) {
-        if (replacedStr[i] !== replacedStr[len-i-1]) {
+        if (replacedStr[i] !== replacedStr[replacedStr.length-i-1] ) {
             return false;
         }
     }
     return true;
-    
-};
+}
