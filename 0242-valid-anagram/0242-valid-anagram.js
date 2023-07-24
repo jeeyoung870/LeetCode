41d9dfd0c25df829c45eec4b06b8b2f196d5aad4
jeeyoung870 <76871404+jeeyoung870@users.noreply.github.com>
2023-07-24 18:02:18 +0900

    Time: 100 ms (35.80%), Space: 48.7 MB (12.69%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.js
+++ 0242-valid-anagram/0242-valid-anagram.js
@@ -13,7 +13,7 @@ var isAnagram = function(s, t) {
     var s_arr = [...s].sort();
     var t_arr = [...t].sort();
     
-    return JSON.stringify(s_arr) === JSON.stringify(t_arr) ;
+    return s_arr.join('') === t_arr.join('') ;
 
 
 };
